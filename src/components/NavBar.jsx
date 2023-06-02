8e9a0145a946c161e584a5aa04f4b5eae3ced982
Erick Uriarte <erickson.1207@gmail.com>
2023-06-01 20:09:56 -0500

    More changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { ShoppingCartIcon} from '@heroicons/react/24/outline'
 
 function NavBar() {
   return (
@@ -17,7 +17,7 @@ function NavBar() {
                 Middle
             </div>
             {/* Right */}
-            <div className="flex items-center">
+            <div className="flex items-center m-4">
                 <div className="pr-4 pl-4">
                     <div className="text-xs xl:text-sm">Hello, sign in</div>
                     <div className="text-sm xl:text-base font-bold">Accounts & Lists</div>
@@ -27,7 +27,10 @@ function NavBar() {
                     <div className="text-sm xl:text-base font-bold">&Orders</div>
                 </div>
                 <div className="flex pr-3 pl-3">
-                    
+                    <ShoppingCartIcon className="h-[48px]"/>
+                    <div className="mt-7 text-xs xl:text-sm font-bold">
+                        Cart
+                    </div>
                 </div>
             </div>
         </div>
