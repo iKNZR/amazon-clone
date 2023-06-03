89c33f1b0dca2310b0649680ec63f72655346a4d
Erick Uriarte <erickson.1207@gmail.com>
2023-06-03 03:50:01 -0500

    Added new functions
    Added:
    -Redux toolkit
    -Reducer

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,12 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { callAPI } from "../utils/CallApi";
 import { ProductDetails } from "./";
 import { GB_CURRENCY } from "../utils/constants";
+import { addToCart } from "../redux/cartSlice";
 
 function ProductPage() {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
+    const [quantity, setQuantity] = useState("1");
+    const dispatch = useDispatch();
 
     const getProduct = () => { 
         callAPI(`data/products.json`)
@@ -14,6 +18,12 @@ function ProductPage() {
             setProduct(productResults[id])
         })
      }
+
+     const addQuantityToProduct = () => {
+      setProduct(product.quantity = quantity);
+      return product;
+    }
+
 
     useEffect(() => {
         getProduct();
@@ -41,18 +51,20 @@ function ProductPage() {
           {/* Right */}
           <div className="col-span-2 p-4 rounded bg-white">
               <div className="text-xl xl:text-2xl text-red-700 font-semibold">{GB_CURRENCY.format(product.price)}</div>
-              <div className="text-base xl:text-lg text-grey-500 font-semibold">RRP: <span className="line-through">{GB_CURRENCY.format(product.oldPrice)}</span></div>
+              <div className="text-base xl:text-lg text-gray-500 font-semibold">RRP: <span className="line-through">{GB_CURRENCY.format(product.oldPrice)}</span></div>
               <div className="text-sm xl:text-base text-blue-500 font-semibold mt-3">FREE Returns</div>
               <div className="text-sm xl:text-base text-blue-500  font-semibold mt-1">FREE Delivery</div>
               <div className="text-base xl:text-lg text-green-700  font-semibold mt-1">In Stock</div>
               <div className="text-base xl:text-lg font-semibold mt-1">Quantity: 
-                  <select className="p-2 bg-white border rounded-md focus:border-indigo-600">
+                  <select onChange={(e) => setQuantity(e.target.value)} className="p-2 bg-white border rounded-md focus:border-indigo-600">
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
                   </select>
               </div>
-              <button className="bg-yellow-400 w-full p-3 text-xs xl-text-sm rounded hover:bg-yellow-500 mt-3">Add to Cart</button>
+              <Link to={"/checkout"}>
+              <button onClick={() => dispatch(addToCart(addQuantityToProduct()))} className="bg-yellow-400 w-full p-3 text-xs xl-text-sm rounded hover:bg-yellow-500 mt-3">Add to Cart</button>
+              </Link>
           </div>
         </div>
       </div>
