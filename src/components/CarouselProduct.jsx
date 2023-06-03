2729f928adee618f8a1b0de9d5c1809532c4de2b
Erick Uriarte <erickson.1207@gmail.com>
2023-06-03 01:42:21 -0500

    Implemented more functionalities
    Implemented:
    - Product Page
    - Search
    - Category

--- src/components/CarouselProduct.jsx
+++ src/components/CarouselProduct.jsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation} from 'swiper';
+import { Link } from 'react-router-dom';
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -17,7 +18,9 @@ function CarouselProduct() {
         {
             Array.from({length: 9}, (_, i) =>
                 <SwiperSlide key={i}>
+                  <Link to={`/product/${i}`}>
                     <img src={`../images/product_${i}_small.jpg`}/>
+                  </Link>
                 </SwiperSlide>
             )
         }
